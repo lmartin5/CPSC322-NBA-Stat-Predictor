c3858a0e091c581998194a60c67ab9f5c77c61cc
Luke Martin <lmartin5@zagmail.gonzaga.edu>
2022-04-19 22:16:54 -0700

    files setup, ready for data prep implement

---
+++ mysklearn/__init__.py
@@ -0,0 +1,9 @@
+"""__init__.py
+@author L. Martin
+@author R. Johnson
+
+Note: this is the package initializer for mysklearn
+__all__ is added onto as we add things to mysklearn for this project
+"""
+
+__all__ = ["myutils", "mypytable", "myclassifiers", "myevaluation"]
